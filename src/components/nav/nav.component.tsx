d87ab658a7dc6d512b18251069f2bcda627a080b
emp74ark <emp74ark@gmail.com>
2023-02-15 15:18:04 +0100

    feat: generate nav links from public routes

--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
+import { navRoutes } from '../../routes/public.routes';
 
 export const NavComponent: FC = () => {
   return(
       <nav>
         <ul>
-          <li><NavLink to={'/records'}>All records</NavLink></li>
-          <li><NavLink to={'/statistics'}>Statistics</NavLink></li>
+          {navRoutes.slice(1).map(({name, path}) => (
+              <li><NavLink to={path}>{name}</NavLink></li>
+          ))}
         </ul>
       </nav>
   )
